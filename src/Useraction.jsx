f3152393549d55dab70334e591abd184b4a2b819
Tei Mensah <twomensah@gmail.com>
2021-08-24 12:00:40 +0100

    editanddelete

--- src/Useraction.jsx
+++ src/Useraction.jsx
@@ -1,9 +1,38 @@
+// export const ADD_USER = "ADD_USER";
+
+// export const addUser = (user) => {
+//   user.id = Math.random().toString();
+//   return {
+//     type: ADD_USER,
+//     payload: user,
+//   };
+// };
+
 export const ADD_USER = "ADD_USER";
+export const EDIT_USER = "EDIT_USER";
+export const DELETE_USER = "DELETE_USER";
 
 export const addUser = (user) => {
   user.id = Math.random().toString();
   return {
     type: ADD_USER,
     payload: user,
+  };
+};
+
+export const EditUser = (user_id, updatedUser) => {
+  updatedUser.id = Math.random().toString();
+
+  return {
+    type: EDIT_USER,
+    user_id: user_id,
+    updatedUser: updatedUser,
+  };
+};
+
+export const DeleteUser = (id) => {
+  return {
+    type: DELETE_USER,
+    payload: id,
   };
 };
